e67979450a89b062fb8893d560bb2e727c8b23ee
Aswin Kumar <aswin.kumar.17@mountblue.tech>
2021-08-27 08:46:06 +0530

    Adding HighCart
    Added highchart to economical bowler, extra given Run team, matched player, matches won, strike rate.

---
+++ src/public/extraRunPerTeam.js
@@ -0,0 +1,76 @@
+(function chart() {
+    fetch('./output/extraRunPerTeam.json')
+        .then((data) => {
+            return data.json();
+        }).then((records) => {
+           const extraScorePerTeam=objectToArray(records);
+            barChart(extraScorePerTeam);
+        })
+        .catch(err => {
+            console.error(err);
+        })
+
+})();
+
+function objectToArray(records){
+    const team = Object.keys(records);
+    const extraScore = Object.values(records);
+    const graphFormat=[];
+    for(let i=0;i<team.length;i++){
+        graphFormat.push([team[i],extraScore[i]]);
+    }
+    return graphFormat;
+}
+
+function barChart(extraScorePerTeam){
+    Highcharts.chart('extraRunPerTeam', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Extra runs conceded per team in the year 2016'
+        },
+        subtitle: {
+            text: 'Source: <a href="http://en.wikipedia.org/wiki/List_of_cities_proper_by_population">Wikipedia</a>'
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+                rotation: -45,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Extra runs'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            pointFormat: 'Extra score in 2016: <b>{point.y:.1f} runs</b>'
+        },
+        series: [{
+            name: 'Extra runs',
+            data: extraScorePerTeam,
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y:.1f}', // one decimal
+                y: 10, // 10 pixels down from the top
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        }]
+    });
+        
+}
---
+++ src/public/matchesPerYear.js
@@ -0,0 +1,75 @@
+(function chart() {
+    let season;
+    let matchesPlayer;
+    fetch('./output/matchesPerYear.json')
+        .then((data) => {
+            return data.json();
+        }).then((records) => {
+            const season = Object.keys(records);
+            const matchesPlayer = Object.values(records);
+            barChart(season, matchesPlayer);
+        })
+        .catch(err => {
+            console.error(err);
+        })
+
+})();
+
+function barChart(season, matchesPlayer) {
+    Highcharts.chart('matchesPerYear', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Number of matches played in IPL from 2008-2017'
+        },
+        subtitle: {
+            text: 'Source: https://www.kaggle.com/manasgarg/ipl">Kaggle.com</a>'
+        },
+        xAxis: {
+            categories: season,
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Matches played',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            valueSuffix: ' matches'
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -40,
+            y: 80,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor:
+                Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+            shadow: true
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: "IPL Records",
+            data: matchesPlayer
+        }],
+    });
+}
